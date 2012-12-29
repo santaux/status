1d57e64bcfcf578b3801042aed3b063468ba6755
konstantin.grabar <santaux@gmail.com>
2012-12-30 01:56:25 +0400

    Small uptime refactoring. Fix grouped uptime calc inaccuracy

--- app/models/project.rb
+++ app/models/project.rb
@@ -39,8 +39,12 @@ class Project < ActiveRecord::Base
     end
   end
 
-  def uptime
-    ((reports.count - reports.failed.count).to_f/reports.count)*100
+  def uptime(period="day")
+    calc_uptime(reports.by_period(period).count, reports.by_period(period).failed.count)
+  end
+
+  def calc_uptime(all_count,failed_count)
+    ((all_count - failed_count).to_f/all_count)*100.to_f
   end
 
   def average_delay_time
@@ -52,7 +56,8 @@ class Project < ActiveRecord::Base
   end
 
   def reports_grouped_response_time(period="day")
-    reports.by_period(period).order("created_at DESC").map { |report| [report.created_at.to_i*1000,report.delay_time.to_f]}.to_s
+    #gr = reports.order("created_at DESC").map{ |r| [r.response_time, r.created_at] }.group_by { |r| r[1].hour }
+    reports.by_period(period).order("created_at DESC").map { |report| [report.created_at.to_i*1000,report.response_time.to_f]}.to_s
   end
 
   def reports_grouped_delay_time(period="day")
@@ -60,12 +65,15 @@ class Project < ActiveRecord::Base
   end
 
   def reports_grouped_uptime(period="day")
-    count = reports.by_period(period).count
-    gr = reports.order("created_at DESC").map{ |r| [r.code, r.created_at] }.group_by { |r| r[1].hour }
+    gr = reports.by_period(period).order("created_at DESC").map{ |r| [r.code, r.created_at] }.group_by { |r| r[1].hour }
     gr.values.map { |v|
+      all_count = v.inject(0) { |sum,i| sum += 1 }
+      failed_count = v.inject(0) { |sum,i| sum += 1 if i[0] != 200; sum}
       [
+        # get first time value to build graphs by time:
         v[0][1].to_i*1000,
-        ((count - v.inject(0) { |sum,i| puts i.inspect; sum += 1 if i[0] != 200; sum}).to_f/count)*100.to_f
+        # get uptime value:
+        calc_uptime(all_count, failed_count)
       ]
     }.sort
   end
