89a35794ea03d10a61bb2411e12bf3d71176f6c2
konstantin.grabar <santaux@gmail.com>
2012-12-30 02:30:58 +0400

    Move some methods to private. Add grouping time choosing

--- app/models/project.rb
+++ app/models/project.rb
@@ -43,10 +43,6 @@ class Project < ActiveRecord::Base
     calc_uptime(reports.by_period(period).count, reports.by_period(period).failed.count)
   end
 
-  def calc_uptime(all_count,failed_count)
-    ((all_count - failed_count).to_f/all_count)*100.to_f
-  end
-
   def average_delay_time
     reports.average(:delay_time)
   end
@@ -57,13 +53,15 @@ class Project < ActiveRecord::Base
 
   def reports_grouped_response_time(period="day")
     #reports.by_period(period).order("created_at DESC").map { |report| [report.created_at.to_i*1000,report.response_time.to_f]}.to_s
-    gr = reports.by_period(period).order("created_at DESC").map{ |r| [r.response_time, r.created_at] }.group_by { |r| r[1].hour }
+    gr = reports.by_period(period).order("created_at DESC").map{ |r|
+      [r.response_time, r.created_at] }.group_by { |r| r[1].send(choose_group_time(period))
+    }
     gr.values.map { |v|
       average = v.map { |r| r[0] }.inject(0.0) { |sum, el| sum + el } / v.size
       [
         # get first time value to build graphs by time:
         v[0][1].to_i*1000,
-        # get average delay time value:
+        # get average response time value:
         average
       ]
     }.sort
@@ -71,7 +69,9 @@ class Project < ActiveRecord::Base
 
   def reports_grouped_delay_time(period="day")
     #reports.by_period(period).order("created_at DESC").map { |report| [report.created_at.to_i*1000,report.delay_time.to_f]}.to_s
-    gr = reports.by_period(period).order("created_at DESC").map{ |r| [r.delay_time, r.created_at] }.group_by { |r| r[1].hour }
+    gr = reports.by_period(period).order("created_at DESC").map{ |r|
+      [r.delay_time, r.created_at] }.group_by { |r| r[1].send(choose_group_time(period))
+    }
     gr.values.map { |v|
       average = v.map { |r| r[0] }.inject(0.0) { |sum, el| sum + el } / v.size
       [
@@ -84,7 +84,9 @@ class Project < ActiveRecord::Base
   end
 
   def reports_grouped_uptime(period="day")
-    gr = reports.by_period(period).order("created_at DESC").map{ |r| [r.code, r.created_at] }.group_by { |r| r[1].hour }
+    gr = reports.by_period(period).order("created_at DESC").map{ |r|
+      [r.code, r.created_at] }.group_by { |r| r[1].send(choose_group_time(period))
+    }
     gr.values.map { |v|
       all_count = v.inject(0) { |sum,i| sum += 1 }
       failed_count = v.inject(0) { |sum,i| sum += 1 if i[0] != 200; sum}
@@ -95,5 +97,24 @@ class Project < ActiveRecord::Base
         calc_uptime(all_count, failed_count)
       ]
     }.sort
+  end
+
+  private
+
+  def calc_uptime(all_count,failed_count)
+    ((all_count - failed_count).to_f/all_count)*100.to_f
+  end
+
+  def choose_group_time(period="day")
+    case period
+    when "day"
+      :hour
+    when "week"
+      :day
+    when "month"
+      :day
+    when "year"
+      :month
+    end
   end
 end
