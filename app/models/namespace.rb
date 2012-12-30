468cbc693dc72987c6b2a1dd7a9575a9965fef97
konstantin.grabar <santaux@gmail.com>
2012-12-30 06:47:54 +0400

    Add average data for namespaces

--- app/models/namespace.rb
+++ app/models/namespace.rb
@@ -4,4 +4,16 @@ class Namespace < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
 
   attr_accessible :name, :description
+
+  def uptime(period="day")
+    projects.inject(0.0) { |sum,i| sum + i.uptime(period) }/projects.count
+  end
+
+  def average_delay_time(period="day")
+    projects.inject(0.0) { |sum,i| sum + i.average_delay_time(period) }/projects.count
+  end
+
+  def average_response_time(period="day")
+    projects.inject(0.0) { |sum,i| sum + i.average_response_time(period) }/projects.count
+  end
 end
