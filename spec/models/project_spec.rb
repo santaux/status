e8c91ada438c5fdf2c1429a7eeac265a4881f103
konstantin.grabar <santaux@gmail.com>
2012-12-30 04:24:18 +0400

    Add project model grouped methods tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,6 +5,14 @@ describe Project do
   let(:namespace) { FactoryGirl.create(:namespace) }
   let(:project) { FactoryGirl.create(:project, :namespace => namespace) }
 
+  before(:each) do
+    @reports = []
+    4.times do |i|
+      @reports << FactoryGirl.create(:report, :project => project)
+    end
+    @reports << FactoryGirl.create(:failed_report, :project => project)
+  end
+
   context "records creating" do
     it "should create project" do
       project.id.should_not be_nil
@@ -18,23 +26,15 @@ describe Project do
     end
 
     it "should have 5 reports" do
-      5.times do |i|
-        FactoryGirl.create(:report, :project => project)
-      end
+      #5.times do |i|
+      #  FactoryGirl.create(:report, :project => project)
+      #end
 
       project.reports.count.should be_equal(5)
     end
   end
 
   context "average methods" do
-    before(:each) do
-      @reports = []
-      4.times do |i|
-        @reports << FactoryGirl.create(:report, :project => project)
-      end
-      @reports << FactoryGirl.create(:failed_report, :project => project)
-    end
-
     it "should have correct uptime" do
       correct_uptime = (4.to_f/5)*100
       project.uptime.should == correct_uptime
@@ -48,6 +48,155 @@ describe Project do
     it "should have correct average_response_time" do
       correct_average_response_time = @reports.inject(0.0) { |sum,r| sum + r.response_time }/5
       ("%.4f" % project.average_response_time ).should == ("%.4f" % correct_average_response_time)
+    end
+  end
+
+  context "group methods" do
+    describe "response time" do
+      it "should have 1 grouped report for day period" do
+        project.reports_grouped_response_time("day").size.should == 1
+      end
+
+      it "should stil have 1 grouped report for day period" do
+        rep = FactoryGirl.create(:report, :project => project)
+        rep.update_attribute(:created_at, Time.now - 2.days)
+        project.reports_grouped_response_time("day").size.should == 1
+      end
+
+      # for day period it groups them by hour
+      it "should have 2 grouped reports for day period" do
+        rep = FactoryGirl.create(:report, :project => project)
+        rep.update_attribute(:created_at, Time.now - 2.hours)
+        project.reports_grouped_response_time("day").size.should == 2
+      end
+
+      # for week period it groups them by day
+      it "should have 3 grouped reports for week period" do
+        2.times do |i|
+          i = i+1
+          rep = FactoryGirl.create(:report, :project => project)
+          rep.update_attribute(:created_at, Time.now - i.days)
+        end
+        project.reports_grouped_response_time("week").size.should == 3
+      end
+
+      # for month period it groups them by day
+      it "should have 4 grouped reports for month period" do
+        3.times do |i|
+          i = i+1
+          rep = FactoryGirl.create(:report, :project => project)
+          rep.update_attribute(:created_at, Time.now - i.weeks)
+        end
+        project.reports_grouped_response_time("month").size.should == 4
+      end
+
+      # for year period it groups them by month
+      it "should have 5 grouped reports for year period" do
+        4.times do |i|
+          i = i+1
+          rep = FactoryGirl.create(:report, :project => project)
+          rep.update_attribute(:created_at, Time.now - i.months)
+        end
+        project.reports_grouped_response_time("year").size.should == 5
+      end
+    end
+
+    describe "delay time" do
+      it "should have 1 grouped report for day period" do
+        project.reports_grouped_delay_time("day").size.should == 1
+      end
+
+      it "should stil have 1 grouped report for day period" do
+        rep = FactoryGirl.create(:report, :project => project)
+        rep.update_attribute(:created_at, Time.now - 2.days)
+        project.reports_grouped_delay_time("day").size.should == 1
+      end
+
+      # for day period it groups them by hour
+      it "should have 2 grouped reports for day period" do
+        rep = FactoryGirl.create(:report, :project => project)
+        rep.update_attribute(:created_at, Time.now - 2.hours)
+        project.reports_grouped_delay_time("day").size.should == 2
+      end
+
+      # for week period it groups them by day
+      it "should have 3 grouped reports for week period" do
+        2.times do |i|
+          i = i+1
+          rep = FactoryGirl.create(:report, :project => project)
+          rep.update_attribute(:created_at, Time.now - i.days)
+        end
+        project.reports_grouped_delay_time("week").size.should == 3
+      end
+
+      # for month period it groups them by day
+      it "should have 4 grouped reports for month period" do
+        3.times do |i|
+          i = i+1
+          rep = FactoryGirl.create(:report, :project => project)
+          rep.update_attribute(:created_at, Time.now - i.weeks)
+        end
+        project.reports_grouped_delay_time("month").size.should == 4
+      end
+
+      # for year period it groups them by month
+      it "should have 5 grouped reports for year period" do
+        4.times do |i|
+          i = i+1
+          rep = FactoryGirl.create(:report, :project => project)
+          rep.update_attribute(:created_at, Time.now - i.months)
+        end
+        project.reports_grouped_delay_time("year").size.should == 5
+      end
+    end
+
+    describe "uptime" do
+      it "should have 1 grouped report for day period" do
+        project.reports_grouped_uptime("day").size.should == 1
+      end
+
+      it "should stil have 1 grouped report for day period" do
+        rep = FactoryGirl.create(:report, :project => project)
+        rep.update_attribute(:created_at, Time.now - 2.days)
+        project.reports_grouped_uptime("day").size.should == 1
+      end
+
+      # for day period it groups them by hour
+      it "should have 2 grouped reports for day period" do
+        rep = FactoryGirl.create(:report, :project => project)
+        rep.update_attribute(:created_at, Time.now - 2.hours)
+        project.reports_grouped_uptime("day").size.should == 2
+      end
+
+      # for week period it groups them by day
+      it "should have 3 grouped reports for week period" do
+        2.times do |i|
+          i = i+1
+          rep = FactoryGirl.create(:report, :project => project)
+          rep.update_attribute(:created_at, Time.now - i.days)
+        end
+        project.reports_grouped_uptime("week").size.should == 3
+      end
+
+      # for month period it groups them by day
+      it "should have 4 grouped reports for month period" do
+        3.times do |i|
+          i = i+1
+          rep = FactoryGirl.create(:report, :project => project)
+          rep.update_attribute(:created_at, Time.now - i.weeks)
+        end
+        project.reports_grouped_uptime("month").size.should == 4
+      end
+
+      # for year period it groups them by month
+      it "should have 5 grouped reports for year period" do
+        4.times do |i|
+          i = i+1
+          rep = FactoryGirl.create(:report, :project => project)
+          rep.update_attribute(:created_at, Time.now - i.months)
+        end
+        project.reports_grouped_uptime("year").size.should == 5
+      end
     end
   end
 end
